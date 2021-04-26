3a121fdeddbcaa26ddf4ae2235845a0a0880fb7a
Alex Araujo <alex.fate2000@gmail.com>
2021-04-26 14:41:29 -0300

    Initial commit

--- repo2/script.py
+++ repo2/script.py
@@ -1,1 +1,9 @@
-import sys
+#!/usr/bin/env python
+
+# Load packages.
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Plot time series.
+plt.plot(np.cumsum(np.random.randn(10000)))
+plt.show()
