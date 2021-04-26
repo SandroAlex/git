02f70863282eb0f6df4b88b062b47c27cb306f81
Alex Araujo <alex.fate2000@gmail.com>
2021-04-26 15:14:18 -0300

    Add stuff

--- repo2/script.py
+++ repo2/script.py
@@ -6,4 +6,8 @@ from matplotlib import pyplot as plt
 
 # Plot time series.
 plt.plot(np.cumsum(np.random.randn(10000)))
+plt.xlabel("Time")
+plt.ylabel("Return")
+plt.title("Asset Evolution")
+plt.grid()
 plt.show()
