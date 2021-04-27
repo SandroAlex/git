36f906ae4edf722cfe0b89f0a2cc831762e58486
Alex Araujo <alex.fate2000@gmail.com>
2021-04-27 14:30:10 -0300

    Add moving average

--- repo2/script.py
+++ repo2/script.py
@@ -2,12 +2,20 @@
 
 # Load packages.
 import numpy as np
+import pandas as pd
 from matplotlib import pyplot as plt
 
 # Plot time series.
-plt.plot(np.cumsum(np.random.randn(10000)))
+data = np.cumsum(np.random.randn(52 * 5 * 30))
+plt.plot(data)
 plt.xlabel("Time")
 plt.ylabel("Return")
 plt.title("Asset Evolution")
 plt.grid()
+
+# Moving average. 
+ma = pd.Series(data=data).rolling(window=5 * 52).mean().values
+plt.plot(ma, color="green")
+
+# Show final figure.
 plt.show()
